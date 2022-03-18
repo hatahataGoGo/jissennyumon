3d0b552147400cd8cc2e5dd648d00e9252de7b98
kaoru <hatahata55aap@gmail.com>
2022-03-19 01:00:58 +0900

    5time tra02.ts commit

--- tra02.ts
+++ tra02.ts
@@ -3,9 +3,12 @@
  * @param num なかなかPushできない 2022/03/18
  * @returns 
  */
-function incriment(num : number){
+function incriment(num: number) {
     return num + 1;
 }
-
+let greet = (name: string) => {
+    return 'hello' + name;
+}
 console.log(incriment(999));
+console.log(greet('999'));
 
